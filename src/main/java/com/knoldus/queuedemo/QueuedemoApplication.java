3e259dadf334394cfc8a5869cf5f40fa2720e74e
Anshika1603 <anshika.srivastava@knoldus.com>
2023-06-20 11:36:16 +0530

    first commit

---
+++ src/main/java/com/knoldus/queuedemo/QueuedemoApplication.java
@@ -0,0 +1,40 @@
+package com.knoldus.queuedemo;
+
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.storage.queue.QueueClient;
+import com.azure.storage.queue.QueueClientBuilder;
+import com.azure.storage.queue.models.SendMessageResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuedemoApplication {
+	static final String queueName = "queue-demo";
+	static final String endpoint = "https://queueanshika.queue.core.windows.net/queue-demo";
+
+	static final String connectionString = "DefaultEndpointsProtocol=https;AccountName=queueanshika;AccountKey=AuFc9sHCrYTfURzjm4+dlv9dzchcgY/nv1JNTdDp4W495rKaxpUCnDiL/OSzfjjf15GjQlyne37Z+AStj/7R6g==;EndpointSuffix=core.windows.net";
+
+	private final static Logger logger = LoggerFactory.getLogger(QueuedemoApplication.class);
+
+
+	public static void main(String[] args) {
+		QueueClient queueClient = new QueueClientBuilder()
+				.endpoint(endpoint)
+				.credential(new DefaultAzureCredentialBuilder().build())
+				.queueName(queueName)
+				.connectionString(connectionString)
+				.buildClient();
+
+		SendMessageResult sendMessageResult = queueClient.sendMessage("Hello world");
+		logger.info("Send message id: {}", sendMessageResult.getMessageId());
+
+		System.out.println("Press any key to end the application");
+		try {
+			System.in.read();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
